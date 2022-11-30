eac220f9f84561c5395de1c8e0aac02bdf7da2a7
Ricardo Gomes <ricardokevi@gmail.com>
2022-11-30 17:40:59 -0300

    Trabalhando com arquivos

---
+++ AppFiles/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace AppFiles
+{
+    class Product
+    {
+        public string Denominacao { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade  { get; set; }
+
+        public Product(string denominacao, double preco, int quantidade)
+        {
+            Denominacao = denominacao;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public void PrecoFinal()
+        {
+            Preco = Quantidade * Preco;
+        }
+
+        public override string ToString()
+        {
+            return Denominacao + ", " + Preco.ToString("F2", CultureInfo.InvariantCulture) ;
+        }
+    }
+}
---
+++ AppFiles/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+
+namespace AppFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string local = @"D:\Aprendendo C#\Files\Arquivo teste\File1.csv";
+            string outLocal = @"D:\Aprendendo C#\Files\Arquivo teste\File2.csv";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(local);
+
+                using (StreamWriter sw = File.AppendText(outLocal))
+                {
+                    foreach(string line in lines)
+                    {
+                        string[] linha = line.Split(",");
+                        string produto = linha[0];
+                        double valor = double.Parse(linha[1], CultureInfo.InvariantCulture);
+                        int quantidade = int.Parse(linha[2]);
+
+                        Product product = new Product(produto, valor, quantidade);
+                        product.PrecoFinal();
+
+                        sw.WriteLine(product);
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Erro no arquivo: ");
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Erro não tratado: " + e.Message);
+            }
+        }
+    }
+}
